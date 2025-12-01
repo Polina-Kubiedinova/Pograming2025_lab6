160fd61a714aa080cf557d60417ab8f179860c82
Polina-Kubiedinova <109977147+Polina-Kubiedinova@users.noreply.github.com>
2025-12-01 04:24:04 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,7 +43,7 @@ resource "aws_security_group" "web_app" {
 
 resource "aws_instance" "webapp_instance" {
   ami             = "ami-0669b163befffbdfc"
-  instance_type   = "t2.micro"
+  instance_type   = "t3.micro"
   vpc_security_group_ids = [aws_security_group.web_app.id]
 
   tags = {
